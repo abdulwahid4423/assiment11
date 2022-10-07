8d05028577f18ee9a8f72f20350d0a4c423b7eec
Abdul Wahid <111135216+abdulwahid4423@users.noreply.github.com>
2022-10-07 05:37:07 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+
+function slide1(){
+    var a = document.getElementById("car")
+    a.src="https://images.unsplash.com/photo-1449426468159-d96dbf08f19f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bW90b3JiaWtlfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
+}
+function slide2(){
+    var a = document.getElementById("car")
+    a.src="https://imgd-ct.aeplcdn.com/370x208/n/cw/ec/40432/scorpio-n-exterior-right-front-three-quarter-15.jpeg?isig=0&q=75"
+}
